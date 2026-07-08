ff6e6f65a22134bea134e35bb5403693dd8bee4f
mj-boop7770 <mrloverflv@gmail.com>
2026-07-08 18:45:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker instalado.');
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', event => {
+  console.log('Service Worker ativo.');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
